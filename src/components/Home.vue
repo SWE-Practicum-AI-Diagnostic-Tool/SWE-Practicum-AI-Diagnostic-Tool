3293634d2c616f6dc3c5dfd151f3bd40f3aea08e
EliCurl <curle4271@my.uwstout.edu>
2025-09-24 15:44:30 -0500

    Playing around with Vue

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script>
+import NaviBar from './NaviBar.vue'
+</script>
+
+<template>
+    <head>
+        <p>
+            Hello World
+        </p>
+        
+        <NaviBar />
+    </head>
+</template>
+
+<style>
+</style>
---
+++ src/components/NaviBar.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+    <button v-on:click="goToHome"> Home </button>
+ </div>
+ <div>
+    <button v-on:click="goToSupport"> Support </button>
+ </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            goToSupport(){
+            },
+            goToHome(){
+            }
+        }
+    }
+</script>
+
+
+<style>
+</style>
