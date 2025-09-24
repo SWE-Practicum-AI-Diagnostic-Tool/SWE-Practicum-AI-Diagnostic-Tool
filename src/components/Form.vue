184e5236656dcf2983dd74f9e1dd666a81e92231
Emmett Jaakkola <emmettdj@gmail.com>
2025-09-24 15:06:49 -0500

    Made boilerplate webpage.

---
+++ src/components/Form.vue
@@ -0,0 +1,26 @@
+<!-- Form -->
+
+<script setup>
+import { FormSection } from './FormSection.vue'
+</script>
+
+<template>
+  <div class="form-container">
+    <form>
+      <!-- Vehicle Make -->
+      <FormSection title="Vehicle Make" />
+      <!-- Vehicle Model -->
+      <FormSection title="Vehicle Model" />
+      <!-- Year of Manufacture -->
+      <FormSection title="Year of Manufacture" />
+      <!-- Mileage -->
+      <FormSection title="Mileage" />
+      <!-- Reported Issues -->
+      <FormSection title="Reported Issues" />
+      <!-- Additional Comments -->
+      <FormSection title="Additional Comments" />
+      <!-- Submit -->
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
---
+++ src/components/FormSection.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="form-group">
+    <label :for="title">{{ title }}</label>
+    <input type="text" :id="title" :name="title" required />
+  </div>
+</template>
