0d25281b4627533794345063b195542b16fe7f8d
EliCurl <curle4271@my.uwstout.edu>
2025-09-26 15:47:52 -0500

    Added Comments to router.js
    Just added some comments of how to add routes

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
+// Import the need page
 import HomeView from './components/Home.vue'
 import FormView from './components/Form.vue'
 
+// This is need to create more routes
+// Create new path and put 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/form', component: FormView },
