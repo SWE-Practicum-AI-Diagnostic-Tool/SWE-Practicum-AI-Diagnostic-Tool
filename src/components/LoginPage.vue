9658e5cae4542f1d28488b172428c5470a97618c
Alekplays <minecraftman581@gmail.com>
2025-09-29 16:04:32 -0500

    Added Google API Login
    Added the google api login to the login page

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -34,6 +34,9 @@
         <!-- Submit -->
         <button type="submit">Log In</button>
       </form>
+      <div style="margin-top: 1rem;">
+        <GoogleLogin :callback="handleGoogleLogin" @error="handleGoogleError" />
+      </div>
     </div>
   </div>
 </template>
@@ -56,6 +59,14 @@ const handleLogin = () => {
     errorMessage.value = 'Invalid email or password.'
   }
 }
+function handleGoogleLogin(response) {
+  console.log('Google Login Success:', response)
+  router.push('/')
+}
+function handleGoogleError(error) {
+  console.error('Google Login Error:', error)
+}
+
 </script>
 
 <style scoped>
