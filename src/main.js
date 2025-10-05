7c34e4290f73188deae80a751df85ec84d35c877
Alekplays <minecraftman581@gmail.com>
2025-10-04 21:11:10 -0500

    Fixed Google Login
    Re-added the Imports on the main file to allow for the Google Login to work

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App);
+app.use(vue3GoogleLogin, {
+  clientId: '847728556581-8per6t3c7po36b8k1q3qko9nvjlg0fai.apps.googleusercontent.com'
+})
 app.use(createPinia());
 app.use(router);
 app.mount('#app');
