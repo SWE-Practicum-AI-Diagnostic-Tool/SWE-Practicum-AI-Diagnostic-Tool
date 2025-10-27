0532adfb0a12fbdea63bb0335311beb6dfeac864
Hyrakan <144388071+Hyrakan@users.noreply.github.com>
2025-10-27 14:35:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    https: {
+      key: './certs/localhost-key.pem',
+      cert: './certs/localhost.pem',
+    }
+  }
 })
