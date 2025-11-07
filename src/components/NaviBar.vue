38c338f40fb69fe77e98fa390ce8089ffea97195
Hyrakan <144388071+Hyrakan@users.noreply.github.com>
2025-11-07 15:50:42 -0600

    created the toggle slider on the NaviBar.vue

--- src/components/NaviBar.vue
+++ src/components/NaviBar.vue
@@ -3,6 +3,7 @@ import { themeColor, siteName } from "../data/items";
 import { RouterLink } from 'vue-router';
 // import { store } from '../store.js'
 import { logout, authState } from '../auth.js'
+
 </script>
 <template>
   <nav class="site-nav dark js-site-navbar mb-5 site-navbar-target">
@@ -23,7 +24,11 @@ import { logout, authState } from '../auth.js'
           class="js-clone-nav 
           -none mt-1 d-lg-inline-block site-menu float-right"
         >
-          <li class="cta-button-outline" style="margin-right: 5px;">
+        <label class="switch">
+          <input type="checkbox">
+          <span class="slider round"></span>
+        </label>
+        <li class="cta-button-outline" style="margin-right: 5px;">
             <RouterLink v-if="!lI" to="/login">Login</RouterLink>
           </li>
           <li class="cta-primary">
@@ -63,3 +68,68 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
